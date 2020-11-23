4a09867a96ba27eeee803f88f8989e26f8987b8e
Roman Ivanov <roman.ivanov.7@gmail.com>
2020-11-23 20:07:00 +0100

    removing extra folders

--- shared-vol/accelerator/training/answers/03_chicago_gmms_01.py
+++
@@ -1,4 +0,0 @@
-from pyspark.ml.feature import VectorAssembler
-
-assembler = VectorAssembler(inputCols=['X Coordinate', 'Y Coordinate'], outputCol='features')
-crime_with_features_sdf = assembler.transform(crime_sdf)
--- shared-vol/accelerator/training/answers/03_classifier_performance.py
+++
@@ -1,5 +0,0 @@
-test_predictions = cv_model.transform(test_sdf)
-
-evaluator = BinaryClassificationEvaluator()
-print('ROC AUC: test', evaluator.evaluate(test_predictions))
-print(evaluator.explainParams())  # Available metrics.
